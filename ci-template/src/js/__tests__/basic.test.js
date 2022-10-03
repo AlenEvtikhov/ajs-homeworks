16dd4d78b18725c0fce15cd67da296863134de0c
AlenEvtikhov <50318546+AlenEvtikhov@users.noreply.github.com>
2022-10-03 14:35:04 +0300

    add test

--- ci-template/src/js/__tests__/basic.test.js
+++ ci-template/src/js/__tests__/basic.test.js
@@ -1,7 +1,35 @@
-import sum from '../basic';
+// import sum from '../basic';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+// test('should sum', () => {
+//   const result = sum([1, 2, 3]);
 
-  expect(result).toBe(6);
+//   expect(result).toBe(6);
+// });
+import characterStatus from '../app';
+
+test('healthy', () => {
+  const character = {
+    name: 'Маг',
+    health: 90,
+  };
+
+  expect(characterStatus(character)).toBe('healthy');
+});
+
+test('wounded', () => {
+  const character = {
+    name: 'Маг',
+    health: 30,
+  };
+
+  expect(characterStatus(character)).toBe('wounded');
+});
+
+test('critical', () => {
+  const character = {
+    name: 'Маг',
+    health: 1,
+  };
+
+  expect(characterStatus(character)).toBe('critical');
 });
--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,6 +1,13 @@
 // TODO: write your code here
-import sum from './basic';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function characterStatus(character) {
+	let status = '';
+	if (character.health > 50) {
+	  status = 'healthy';
+	} else if (character.health > 14 && character.health < 51) {
+	  status = 'wounded';
+	} else if (character.health < 15) {
+	  status = 'critical';
+	}
+	return status;
+ }
